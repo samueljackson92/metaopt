464afb0d2c6c3ebb0fd22a39db890953e5c7fae5
Samuel Jackson <samueljackson@outlook.com>
2016-08-28 21:01:02 +0100

    Add test for hyperparameters

--- src/test/SwarmTest.cpp
+++ src/test/SwarmTest.cpp
@@ -61,3 +61,29 @@ TEST_CASE("Swarm Optimize Rosenbrock", "[Swarm.Optimize]") {
   REQUIRE(solution.at("x1") == Approx(1).epsilon(0.1));
   REQUIRE(solution.at("x2") == Approx(1).epsilon(0.1));
 }
+
+TEST_CASE("Swarm Optimize HyperParams", "[Swarm.Optimize]") {
+  Parameters hyperParams = makeHyperParameters();
+  // A larger Omega parameter leads to faster convergence on the
+  // correct parameter setting (1, 1, 1)
+  hyperParams["omega"] = 0.5;
+  hyperParams["phi_local"] = 0.1;
+  hyperParams["phi_global"] = 0.1;
+
+  ParameterSpace parameters;
+  parameters["x0"] = std::make_pair(-3, 3);
+  parameters["x1"] = std::make_pair(-3, 3);
+  parameters["x2"] = std::make_pair(-3, 3);
+
+  Swarm sw(parameters, hyperParams, 10, 50);
+
+  CostFunction func = rosen;
+  REQUIRE_NOTHROW(sw.optimize(func));
+  auto solution = sw.getBestParameters();
+
+  REQUIRE(func(solution) == Approx(0).epsilon(0.0001));
+  REQUIRE(solution.size() == 3);
+  REQUIRE(solution.at("x0") == Approx(1).epsilon(0.001));
+  REQUIRE(solution.at("x1") == Approx(1).epsilon(0.001));
+  REQUIRE(solution.at("x2") == Approx(1).epsilon(0.001));
+}
